da8b034482bcc9d6081eb82860e523fea1430c84
Hoa Tử Sắc <ard_nguyen@outlook.com.vn>
2023-11-15 08:29:28 +0700

    set up frontend

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import Mapbox from './components/Mapbox/Mapbox'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Mapbox />
+    </>
+  )
+}
+
+export default App
---
+++ front-end/src/components/Mapbox/Mapbox.jsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect, useState } from 'react';
+//import ReactMapboxGl from "react-mapbox-gl";
+import { token } from "../../constains/token";
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+import './mapbox.css'
+
+mapboxgl.accessToken = token;
+
+const Mapbox = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v12',
+          center: [lng, lat],
+          zoom: zoom
+        });
+    });
+
+    return (
+        <div>
+            <div ref={mapContainer} className='container'/>
+        </div>
+    )
+}
+
+export default Mapbox
