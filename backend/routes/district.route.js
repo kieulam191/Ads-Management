252c08ad9a4e4810ee837f83ce8649ac9ba357a0
DESKTOP-D7CDNGM\Minh <vocongminh3@gmail.com>
2023-12-24 22:06:34 +0700

    quản lý quận huyện tỉnh thành

---
+++ backend/routes/district.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as districtModel from '../models/district.model.js'
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    let provinceCode = req.body.provinceCode || 79;
+    let page = req.body.page || 0;
+    const data = await districtModel.getByProvince(provinceCode,page);
+    res.status(200).json({
+        data,
+    });
+});
+
+export default router;
