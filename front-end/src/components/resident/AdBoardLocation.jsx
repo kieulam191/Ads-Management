9f76d948986fbd6945225c9587c1a1a01bb8c700
KIEU QUANG LAM <kieulam191@gmail.com>
2024-01-04 11:24:05 +0700

    fix after call API

--- front-end/src/components/resident/AdBoardLocation.jsx
+++ front-end/src/components/resident/AdBoardLocation.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdBoardInfo from "./AdBoard";
+import { useLocation } from "react-router-dom";
+import axios from "../../services/api";
 import "./AdboardLocation.css";
 
 const AdBoardListLocation = () => {
+  const location = useLocation();
+  const [adBoard, setAdBoard] = useState([]);
+  useEffect(() => {
+    const wards = location.state.ward;
+    const districts_fullname = location.state.district;
+
+    const body = { wards: [wards], districts_fullname };
+    axios.post("/advertisinglocations", body).then((res) => {
+      setAdBoard(res.data);
+    });
+  }, []);
   return (
     <div>
       <div>
-        <AdBoardInfo />;
-        <AdBoardInfo />;
+        {adBoard.map((ad) => (
+          <AdBoardInfo key={ad.location_id} ad={ad} />
+        ))}
       </div>
     </div>
   );
