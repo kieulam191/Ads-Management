458a854dadf5c3186b2de6ed765d0f1ac69b7d02
DESKTOP-D7CDNGM\Minh <vocongminh3@gmail.com>
2024-01-06 18:30:16 +0700

    bảng quảng cáo mới

---
+++ backend/models/advertisingBoard.model.js
@@ -0,0 +1,37 @@
+import db from '../db/db.js'
+
+const TABLE = 'advertising_board'
+
+export async function findAll(page) {
+    return await db(TABLE)
+                .where("is_delete", "<>", true)
+                .select();
+}
+
+export async function findByAdPlaId(id) {
+    return await db(TABLE)
+                .leftJoin("adstabletype", function(){
+                    this
+                    .on("adstabletype.id","advertising_board.adstabletype_id")  // Loại vị trí
+                    .onVal("adstabletype.is_delete",false)
+
+                })
+                .leftJoin("ads_company",function ()   //Hình thức quảng cáo
+                {
+                    this
+                    .on("ads_company.id","advertising_board.ads_company_id")
+                    .onVal("ads_company.is_delete",false)   
+                })
+                .where("advertising_board.is_delete", false)
+                .andWhere("advertising_board.advertising_placement_id", id)
+                .select({id : "advertising_board.id",
+                adstabletype_name : "adstabletype.name",
+                height : "advertising_board.height",
+                width : "advertising_board.width",
+                url : "advertising_board.url",
+                start_date : "advertising_board.start_date",
+                end_date : "advertising_board.end_date",
+                ads_company_id : "ads_company.id"
+                });
+                
+}
---
+++ backend/routes/advertisingBoard.route.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import * as advertisingBoardModel from '../models/advertisingBoard.model.js'
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try{
+    let wards = req.body.wards;
+    let districts_fullname = req.body.districts_fullname;
+    const data = await advertisingPlacementModel.findAll(wards,districts_fullname);
+
+    return res.status(200).json(data);
+    }
+    catch(e)
+    {
+        console.log(e.stack);
+        return res.status(400);
+    }
+});
+
+router.post('/add', async (req, res) => {
+    try{
+        const data = await advertisinglocationsModel.add(req.body);
+        return res.status(200).json({msg:"Success"});
+    }
+    catch(e)
+    {
+        console.log(e.stack);
+        return res.status(400);
+    }
+});
+
+router.post('/id', async (req,res) => {
+    try{
+        
+        let advertising_placement_id = req.body.advertising_placement_id || 0;
+        if(advertising_placement_id != 0)
+        {
+            const data = await advertisingBoardModel.findByAdPlaId(advertising_placement_id);
+            return res.status(200).json(data);
+        }
+    }
+    catch(e)
+    {
+        console.log(e.stack);
+        return res.status(400);
+    }
+    
+});
+
+router.post('/update', async (req,res) => {
+    try{
+        let id = req.body.id || 0;
+        let data = req.body.data || {};
+        await advertisingPlacementModel.update(id,data);
+        return res.status(200).json({msg:"Success"});
+    }
+    catch(e)
+    {
+        console.log(e.stack);
+        return res.status(400);
+    }
+});
+
+
+export default router;
